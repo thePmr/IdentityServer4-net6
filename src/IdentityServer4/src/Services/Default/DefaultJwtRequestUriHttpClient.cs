bf1e4da0ae57191071d6f41aa38b9a8cc9f3c4a1
Alexander Goldabin <alexg@jibble.io>
2022-10-27 12:28:15 +0700

    Few more updates on Depricated things

--- src/IdentityServer4/src/Services/Default/DefaultJwtRequestUriHttpClient.cs
+++ src/IdentityServer4/src/Services/Default/DefaultJwtRequestUriHttpClient.cs
@@ -3,6 +3,7 @@
 
 
 using System;
+using System.Collections.Generic;
 using IdentityServer4.Models;
 using Microsoft.Extensions.Logging;
 using System.Net.Http;
@@ -39,7 +40,7 @@ namespace IdentityServer4.Services
         public async Task<string> GetJwtAsync(string url, Client client)
         {
             var req = new HttpRequestMessage(HttpMethod.Get, url);
-            req.Properties.Add(IdentityServerConstants.JwtRequestClientKey, client);
+            req.Options.Set(new HttpRequestOptionsKey<Client>(IdentityServerConstants.JwtRequestClientKey), client);
 
             var response = await _client.SendAsync(req);
             if (response.StatusCode == System.Net.HttpStatusCode.OK)
